a20c5bcc622dfff3f9368f672d8ec7bafb70c5c6
furedea <132188853+furedea@users.noreply.github.com>
2026-05-18 16:54:07 +0900

    ci: add cargo deny audit

--- flake.nix
+++ flake.nix
@@ -15,6 +15,7 @@
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           cargo
+          cargo-deny
           cargo-machete
           clippy
           commitlint

dd632849173533bd3dc19c7023af14cc9b955a3d
Kaito Shigyo / 執行凱斗 <132188853+furedea@users.noreply.github.com>
2026-05-18 19:04:06 +0900

    ci: add cargo machete audit (#2)

--- flake.nix
+++ flake.nix
@@ -15,6 +15,7 @@
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           cargo
+          cargo-machete
           clippy
           commitlint
           deadnix
